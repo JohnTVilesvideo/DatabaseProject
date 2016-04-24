b9a52d9574e1f267767e2615667dd02b987558bc
Amrit Dhakal <mail@amritdhakal.com>
2016-04-24 11:06:06 -0400

    Addded edit functionality to profile page and fixed login page.

--- Bootstrap/admin/nav.php
+++ Bootstrap/admin/nav.php
@@ -34,7 +34,7 @@ if (array_key_exists('user_id', $_SESSION)) {
             <ul class="nav navbar-nav navbar-right">
                 <?php
                 if ($isLoggedIn) {
-                    printf("<p class='navbar-text'>You are logged in as</p><li><a href='profile.php'>$name</a></li>");
+                    printf("<p class='navbar-text'>You are logged in as</p><li><a href='../profile.php'>$name</a></li>");
                     printf("<a href='logout.php'><button type='button' onclick='' class='btn btn-default navbar-btn'>Logout</button></a>");
                 } else {
                     printf("<a href='login.php'><button type='button' class='btn btn-default navbar-btn'>Log in</button></a>");
